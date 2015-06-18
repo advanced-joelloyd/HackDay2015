272ca96fea5e98f1dd0be747001135dd429f3b1a
Joe Lloyd <joe.lloyd@advancedcomputersoftware.com>
2015-06-18 17:52:32 +0100

    Added basic web api with simple GET requests.

---
+++ Server.App/Server.App/Messsages/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Server.App.Messsages
+{
+    [RoutePrefix("messages")]
+    public class MessageController : ApiController
+    {
+        private const string TheMessage = "Hi Mum";
+
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return TheMessage;
+        }
+
+        [HttpGet]
+        [Route("{count:int:min(1)?}")]
+        public IEnumerable<string> Get(int count)
+        {
+            for (var i = 1; i <= count; i++)
+            {
+                yield return TheMessage;
+            }
+        }
+    }
+}
